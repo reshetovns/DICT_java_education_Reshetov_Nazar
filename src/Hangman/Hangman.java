f725495cec2df7f308b65c3bac804470938b318a
reshetovns <n.s.reshetov@student.khai.edu>
2021-12-28 01:29:33 +0200

    Hangman stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,28 +1,112 @@
 package Hangman;
-
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 public class Hangman {
 
+    String[] words = {"java", "kotlin", "python", "javascript"};
+    int lives = 8;
+    String word = "";
+    StringBuilder usedChars = new StringBuilder("");
+    StringBuilder secretWord = new StringBuilder("");
+    Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String[] words = {"java", "kotlin", "python", "javascript"};
-        Random randomIntGen = new Random();
-        int randomInt = randomIntGen.nextInt(words.length);
-        String word = words[randomInt];
-        StringBuilder hiddenWord = new StringBuilder(word.substring(0, 2));
-        for (int i = 0; i < word.length() - 2; i++){
-            hiddenWord.append('-');
+        Hangman myGame = new Hangman();
+        myGame.startGame();
+    }
+
+
+
+    public void startGame(){
+        while (true) {
+            System.out.println("HANGMAN");
+            System.out.println("Type \"play\" to play the game or \"exit\" ");
+            String choice = input.nextLine();
+            if (Objects.equals(choice, "play")){
+                setRandomWord();
+                createSecretWord();
+                while (true) {
+                    if (lives == 0) {
+                        System.out.println("You lost!");
+                        break;
+                    } else {
+                        if (secretWord.indexOf("-") != -1) {
+                            makeMove();
+                        } else {
+                            System.out.println(secretWord);
+                            System.out.println("You win!");
+                            break;
+                        }
+                    }
+                }
+                lives = 8;
+                word = "";
+                usedChars = new StringBuilder("");
+                secretWord = new StringBuilder("");
+
+            } else if (Objects.equals(choice, "exit")) {
+                break;
+            }
         }
-        System.out.println("HANGMAN");
-        System.out.printf("Guess the word %s: ", hiddenWord);
-        Scanner userInput = new Scanner(System.in);
-        String answer = userInput.nextLine();
-        if (answer.equals(word)) {
-            System.out.println("You survived!");
+    }
+
+    public void makeMove() {
+        System.out.printf("Guess the word %s:", secretWord);
+        String choice = input.nextLine();
+        if (checker(choice)) {
+            usedChars.append(choice);
+            if (!word.contains(choice)) {
+                System.out.println("The letter doesn't appear in word");
+                --lives;
+            } else {
+                updateSecretWord(choice);
+            }
+        }
+    }
+
+    public void setRandomWord() {
+        Random myRandom = new Random();
+        int randomInt = myRandom.nextInt(words.length);
+        word = words[randomInt];
+    }
+
+    public void createSecretWord() {
+        for (int i = 0; i < word.length(); i++) {
+            secretWord.append('-');
+        }
+    }
+
+
+    public void updateSecretWord(String choice) {
+        int index = 0;
+        while (index >= 0) {
+            index = word.indexOf(choice, index);
+            if (index >= 0) {
+                secretWord.setCharAt(index, choice.charAt(0));
+                ++index;
+            }
+        }
+    }
+
+    public Boolean checker (String choice) {
+        Pattern p = Pattern.compile("[A-Za-z]");
+        if (choice.length() != 1) {
+            System.out.println("You should enter one letter");
+            return false;
+        }
+        else if (!p.matcher(choice).matches()){
+            System.out.println("You should enter only letters!");
+            return false;
+        }
+        else if (usedChars.indexOf(choice) != -1){
+            System.out.println("You already choose that letter");
+            return false;
         }
         else {
-            System.out.println("You lost");
+            return true;
         }
     }
 }
