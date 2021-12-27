7d40e9ffac07ba222d4189a07b966cf2690143c0
reshetovns <n.s.reshetov@student.khai.edu>
2021-12-28 00:39:02 +0200

    Hangman stage 1

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN\n"+
+                "The game will be available soon.");
+    }
+}
